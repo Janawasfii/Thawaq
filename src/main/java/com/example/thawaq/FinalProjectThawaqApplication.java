af51342981ca54afae3017318c05a508cff3167b
Jana Wafi <janawas1@hotmail.com>
2024-09-18 08:06:04 +0300

    Add files via upload

---
+++ src/main/java/com/example/thawaq/FinalProjectThawaqApplication.java
@@ -0,0 +1,13 @@
+package com.example.thawaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectThawaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectThawaqApplication.class, args);
+	}
+
+}
